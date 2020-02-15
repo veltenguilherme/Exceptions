d371ccdd80985c665a72e5c23bbdd04af1127c57
Guilherme H. Velten <guilherme-velten@live.com>
2020-02-15 10:16:33 -0300

    commit

---
+++ Exceptions/Ex.cs
@@ -0,0 +1,11 @@
+﻿namespace Exceptions
+{
+    public static class Ex
+    {
+        public enum EnmException
+        {
+            INVALID_OBJECT = 1,
+            INVALID_TOKEN = 2
+        }
+    }
+}
